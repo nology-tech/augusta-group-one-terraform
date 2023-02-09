e78d5df82723d192dfa61e4a1e600c1ea37546b7
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2023-02-09 17:43:31 +0000

    terraform pipeline with jenkins

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
 # changes made = 'richardgurney' -> 'groupone'
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 
 provider "aws" {
   region     = var.AWS_DEFAULT_REGION
